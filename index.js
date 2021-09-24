247dcc01ef711169c8697433f65483fe8a22dfc5
mukeshdani <mukeshdani00@gmail.com>
2021-09-24 14:12:19 +0530

    add event listner in all button

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 
-document.querySelector("button").removeEventListener("click",function(){
-    alert("i got selected");
-})
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < numberOfDrumButtons; i++) {
+
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+
+
+    alert("i got selected ");
+
+  });
+
+}
